33fb7d863ae846b234ea005c66f095d66df7362f
Hazem Krimi <me@hazemkrimi.tech>
2026-04-20 21:28:26 +0100

    chore: handle other token types and special symbols

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,27 +1,45 @@
 package parser
 
 import (
+	"strings"
+
 	"github.com/hazemKrimi/jack-compiler/internal/tokenizer"
 )
 
 func ParseTokens(tokens []tokenizer.Token) string {
-	output := "<tokens>\n"
+	var output strings.Builder
+
+	output.WriteString("<tokens>\n")
 
 	for _, token := range tokens {
 		switch token.Type {
 		case tokenizer.SYMBOL:
-			{
-				output += "<symbol>" + token.Value + "</symbol>\n"
-			}
-		case tokenizer.KEYWORD:
-			{
-				output += "<keyword>" + token.Value + "</keyword>\n"
+			var value string
+
+			switch token.Value {
+			case "<":
+				value = "&lt;"
+			case ">":
+				value = "&gt;"
+			case "&":
+				value = "&amp;"
+			default:
+				value = token.Value
 			}
 
+			output.WriteString("<symbol> " + value + " </symbol>\n")
+		case tokenizer.KEYWORD:
+			output.WriteString("<keyword> " + token.Value + " </keyword>\n")
+		case tokenizer.IDENTIFIER:
+			output.WriteString("<identifier> " + token.Value + " </identifier>\n")
+		case tokenizer.INT_CONST:
+			output.WriteString("<integerConstant> " + token.Value + " </integerConstant>\n")
+		case tokenizer.STR_CONST:
+			output.WriteString("<stringConstant> " + token.Value + " </stringConstant>\n")
 		}
 	}
 
-	output += "</tokens>\n"
+	output.WriteString("</tokens>\n")
 
-	return output
+	return output.String()
 }
