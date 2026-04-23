c32e3fd4e27e7adb90482490a055e9ba90255e2d
Hazem Krimi <me@hazemkrimi.tech>
2026-04-23 18:27:24 +0100

    chore: rename compilation engine to parser

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/hazemKrimi/jack-compiler/internal/compilation-engine"
+	"github.com/hazemKrimi/jack-compiler/internal/parser"
 	"github.com/hazemKrimi/jack-compiler/internal/tokenizer"
 )
 
